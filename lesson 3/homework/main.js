322bbe33ce7920163cf41a51be1bcff56ecb5054
VadymChink <vadim-vsnnschuk@ro.ru>
2022-02-07 13:14:36 +0200

    end homework lesson 3  for js

--- lesson 3/homework/main.js
+++ lesson 3/homework/main.js
@@ -155,7 +155,7 @@ for (let i = 0; i < 100; i++) {
 // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
 
 for (let i = 0; i < 100; i++) {
-    if (i % 2 === 1) {
+    if (i % 2 !== 0) {
         document.write( i + " ");
     }
 }
