cc7b45ee3bb1a763558f0424b82da1ad17b72f6a
VadymChink <vadim-vsnnschuk@ro.ru>
2022-03-21 17:22:41 +0200

    end additional lesson 13 for js

--- lesson 13/main.js
+++ lesson 13/main.js
@@ -172,22 +172,53 @@
 //     })
 //
 
-let point1 = 'Прокинусь'
-let point2 = 'Встати'
-let point3 = 'Ранкова гігієна'
-let point4 = 'Ранкова кава'
-let point5 = 'Сніданок'
-let point6 = 'Прибирання кімнати та робочого місця'
-let point7 = 'Зробити домашню роботу по лекції 13'
-let point8 = 'Обід'
-let point9 = 'Прогулянка на свіжому повітрі. Закупи продуктів'
-let point10 = 'Зробити класну роботу по лекції 13'
-let point11 = 'Вечеря'
-let point12 = 'Читати книгу мінімум годину'
-let point13 = 'Подивитись відео з запланованого списку'
-let point14 = 'Вечірні гігієнічні процедури'
-let point15 = 'Фільм або серіальчик перед сном'
-let point16 = 'Заснути'
+// =========Promise===============
+
+// let point1 = 'Прокинусь'
+// let point2 = 'Встати'
+// let point3 = 'Ранкова гігієна'
+// let point4 = 'Ранкова кава'
+// let point5 = 'Сніданок'
+// let point6 = 'Прибирання кімнати та робочого місця'
+// let point7 = 'Зробити домашню роботу по лекції 13'
+// let point8 = 'Обід'
+// let point9 = 'Прогулянка на свіжому повітрі. Закупи продуктів'
+// let point10 = 'Зробити класну роботу по лекції 13'
+// let point11 = 'Вечеря'
+// let point12 = 'Читати книгу мінімум годину'
+// let point13 = 'Подивитись відео з запланованого списку'
+// let point14 = 'Вечірні гігієнічні процедури'
+// let point15 = 'Фільм або серіальчик перед сном'
+// let point16 = 'Заснути'
+
+// function newPromise(point) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log(point);
+//             resolve();
+//         }, 2000)
+//     });
+// }
+//
+// newPromise(point1)
+// .then(() => newPromise(point2))
+// .then(() => newPromise(point3))
+// .then(() => newPromise(point4))
+// .then(() => newPromise(point5))
+// .then(() => newPromise(point6))
+// .then(() => newPromise(point7))
+// .then(() => newPromise(point8))
+// .then(() => newPromise(point9))
+// .then(() => newPromise(point10))
+// .then(() => newPromise(point11))
+// .then(() => newPromise(point12))
+// .then(() => newPromise(point13))
+// .then(() => newPromise(point14))
+// .then(() => newPromise(point15))
+// .then(() => newPromise(point16));
+
+
+// ====================async/await=====================
 
 function newPromise(point) {
     return new Promise((resolve) => {
@@ -197,20 +228,26 @@ function newPromise(point) {
         }, 2000)
     });
 }
+let point1 = ['Прокинусь',
+    'Встати',
+    'Ранкова гігієна',
+    'Ранкова кава',
+    'Сніданок',
+    'Прибирання кімнати та робочого місця',
+    'Зробити домашню роботу по лекції 13',
+    'Обід',
+    'Прогулянка на свіжому повітрі. Закупи продуктів',
+    'Зробити класну роботу по лекції 13',
+    'Вечеря',
+    'Читати книгу мінімум годину',
+    'Подивитись відео з запланованого списку',
+    'Вечірні гігієнічні процедури',
+    'Фільм або серіальчик перед сном',
+    'Заснути']
 
-newPromise(point1)
-.then(() => newPromise(point2))
-.then(() => newPromise(point3))
-.then(() => newPromise(point4))
-.then(() => newPromise(point5))
-.then(() => newPromise(point6))
-.then(() => newPromise(point7))
-.then(() => newPromise(point8))
-.then(() => newPromise(point9))
-.then(() => newPromise(point10))
-.then(() => newPromise(point11))
-.then(() => newPromise(point12))
-.then(() => newPromise(point13))
-.then(() => newPromise(point14))
-.then(() => newPromise(point15))
-.then(() => newPromise(point16));
+async function fun(arg) {
+    for (let string of arg) {
+        await newPromise(string)
+    }
+}
+    fun(point1);
